9c393438703473e18156c6496f8237c04dfe1ba8
Joseph Bakouny <Youssef.Bakouny@usj.edu.lb>
2021-10-07 14:23:08 +0300

    Implement product and sum in terms of mapReduce

--- src/main/java/mapreduce/Main.java
+++ src/main/java/mapreduce/Main.java
@@ -5,38 +5,24 @@ import java.util.function.Function;
 
 public class Main {
 
-//    public static int mapReduce(int zero,
-//                                 BiFunction<Integer, Integer, Integer> op,
-//                                 Function<Integer, Integer> f,
-//                                 int a,
-//                                 int b) {
-//        while(a <= b) {
-//            zero = op.apply(zero, f.apply(a));
-//            ++a;
-//        }
-//        return zero;
-//    }
-    //    def product(f : Int => Int, a : Int, b : Int): Int =
-//    mapReduce(1, (x, y) => x * y, f, a, b)
-//
-
-
-    static int product(Function<Integer, Integer> f, int a, int b) {
-        if (a > b) {
-            return 1;
-        } else {
-            return f.apply(a) * product(f, a + 1, b);
+    static int mapReduce(int zero,
+                                BiFunction<Integer, Integer, Integer> op,
+                                Function<Integer, Integer> f,
+                                int a,
+                                int b) {
+        while(a <= b) {
+            zero = op.apply(zero, f.apply(a));
+            ++a;
         }
+        return zero;
     }
 
-    //    def sum (f : Int => Int, a : Int, b : Int): Int =
-//    mapReduce(0, (x, y) => x + y, f, a, b)
+    static int product(Function<Integer, Integer> f, int a, int b) {
+        return mapReduce(1, (x, y) -> x * y, f, a, b);
+    }
+
     static int sum(Function<Integer, Integer> f, int a, int b) {
-        if (a > b) {
-            return 0;
-        } else {
-            return f.apply(a) + sum(f, a + 1, b);
-        }
+        return mapReduce(0, Integer::sum, f, a, b);
     }
 
     public static int sumInts(int a, int b) {
